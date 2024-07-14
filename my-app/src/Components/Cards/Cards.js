9f1b254b51d6cb11bdd4c9f870eb4d123325e240
hasanhameed <hasanhameed858@gmail.com>
2024-07-14 22:18:29 +0530

    Induvidual product page updated

--- my-app/src/Components/Cards/Cards.js
+++ my-app/src/Components/Cards/Cards.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Cards.css';
 
 const Cards = ({ item, handleClick }) => {
-  const { title, price, imageUrl, quantity } = item;
+  const { id, title, price, imageUrl, quantity } = item;
 
   return (
     <div className='cards'>
       <div>
-       <img src={imageUrl} alt={title} />
+        <img src={imageUrl} alt={title} />
       </div>
       <section>
         <p>{title}</p>
         <p>Rs.{price}</p>
         <p>{quantity}</p>
-        <button onClick={()=>handleClick(item)}>Add to Cart</button>
+        <button onClick={() => handleClick(item)}>Add to Cart</button>
+        <Link to={`/product/${id}`} state={{ item }}>View Product</Link>
       </section>
     </div>
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
@@ -10,46 +10,26 @@ import { AuthContextProvider } from './Authentication/src/Store.js/auth-context'
 import AuthPage from './Authentication/src/pages/AuthPage';
 import UserProfile from './Authentication/src/components/Profile/UserProfile';
 import HomePage from './Authentication/src/pages/HomePage'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/Home',
-    element: <Home />
-  },
-  {
-    path: '/About',
-    element: <About />
-  }, {
-    path: '/Login',
-    element: <AuthPage />
-  },
-  {
-    path: '/Report',
-    element: <Report />
-  },
-  {
-    path: '/auth',
-    element: <AuthPage />
-  },
-  {
-    path: '/profile',
-    element: < UserProfile />
-  },
-  {
-    path: '/HomaPage',
-    element: <HomePage/>
-  },
-]);
-
+import ProductList from './Pages/ProductPage/Product';
+import Product from './Pages/ProductPage/Product';
 ReactDOM.render(
-  <AuthContextProvider> 
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <AuthContextProvider>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}  />
+          <Route path="/Home"  element={<Home />}  />
+          <Route path="/About" element={<About />}  />
+          <Route path="/Login" element={<AuthPage />} />
+          <Route path="/Report"  element={<Report />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile"  element={<UserProfile />}  />
+          <Route path="/HomePage" element={<HomePage />}/>
+          <Route path="/products"  element={<ProductList />}  />
+          <Route path="/product/:id"  element={<Product />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
   </AuthContextProvider>,
   document.getElementById('root')
 );
